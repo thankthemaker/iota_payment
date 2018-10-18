8d9a6f0a214b227c134b16b019f0501f571e19ca
dgey <tonymande@gmx.de>
2018-10-18 22:25:40 +0200

    oled

--- src/iota_terminal.py
+++ src/iota_terminal.py
@@ -21,7 +21,7 @@ from keypad import keypad
 
 # rev.1 users set port=0
 # substitute spi(device=0, port=0) below if using that interface
-serial = i2c(port=0, address=0x3C)
+serial = i2c(port=1, address=0x3C)
 
 # substitute ssd1331(...) or sh1106(...) below if using that device
 device = ssd1306(serial)
