b450ddfb15764aaab56036c4996d1468c962349d
dgey <tonymande@gmx.de>
2019-07-10 09:55:08 +0200

    first layout

--- electronjs/paymentsystem/src/app/home/home.page.ts
+++ electronjs/paymentsystem/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ToastController } from '@ionic/angular';
+import QRCode from 'qrcode'
 
 @Component({
   selector: 'app-home',
@@ -8,9 +9,18 @@ import { ToastController } from '@ionic/angular';
 })
 export class HomePage {
 
+  qrImage = '';
+  address = 'PCCARCHBFN9SPFYSHLTLCLTLPJSDDDDQIPTBGUZPEQLHDMQYBRC9FYFUNSNA9DKLKYN9R9KRGKMFHFMX9RWYYUDPKZ';
+
   constructor(public toastController: ToastController) {}
 
+  ionViewDidEnter() {
+    this.process();
+    this.displayQrCode();
+  }
+
   async presentToast() {
+    this.process();
     const toast = await this.toastController.create({
       message: 'Your settings have been saved.',
       duration: 2000
@@ -18,4 +28,15 @@ export class HomePage {
     toast.present();
   }
 
+  displayQrCode() {
+    return this.qrImage !== '';
+  }
+
+  process() {
+    const qrcode = QRCode;
+    const self = this;
+    qrcode.toDataURL(self.address, { errorCorrectionLevel: 'H' }, function (err, url) {
+      self.qrImage = url;
+    })
+  }
 }
