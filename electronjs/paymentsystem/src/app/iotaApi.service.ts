383b3e197b78ed97392742981213d801b8dd4bb4
Guy Küstermann <guy.kuestermann@postbank.de>
2019-08-19 21:57:29 +0200

    api-key

--- electronjs/paymentsystem/src/app/iotaApi.service.ts
+++ electronjs/paymentsystem/src/app/iotaApi.service.ts
@@ -3,20 +3,26 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 const baseUrl = 'https://iota-api.thank-the-maker.org';
 
+const options = {
+    headers: {
+        'x-api-key': 'iotapoc-88gbFGg8HoPn2LyNvZcac3NJudd'
+    }
+};
+
 @Injectable()
 export class IotaApiService {
     constructor(private http: HttpClient) {
     }
 
     getNewAddress() {
-        return this.http.post<any>(baseUrl + '/addresses', null);
+        return this.http.post<any>(baseUrl + '/addresses', null, options);
     }
 
     getAddressInfo(address) {
-        return this.http.get<any>(baseUrl+ '/addresses' + '/' + address)
+        return this.http.get<any>(baseUrl+ '/addresses' + '/' + address, options)
     }
 
     getSeedInfo() {
-        return this.http.get<any>(baseUrl+ '/seeds' + '/' + '1')
+        return this.http.get<any>(baseUrl+ '/seeds' + '/' + '1', options)
     }
 }
